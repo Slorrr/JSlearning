7d418d82e2fccb6193abb75ea605990255953617
Slorrr <22tamikill11@gmail.com>
2022-08-22 19:45:04 +0300

    test function expression

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-function showMessage(from, text) { // аргументы: from, text
-    alert(from + ': ' + text);
-  }
-  
-  showMessage('Никита', 'Привет!'); // Никита: Привет! (*)
-  showMessage('Никита', "Как дела?"); // Никита: Как дела? (**)
+let sayHi = function() {
+  alert( "Привет" );
+};
