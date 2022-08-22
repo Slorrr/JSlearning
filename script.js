a2974a68883ae9984f2b2823398fd13b1a406fce
Slorrr <22tamikill11@gmail.com>
2022-08-22 20:10:56 +0300

    function declaration and function expression

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+sayBye()//Выполнится 
+sayHi()//Не выполнится
+
+//Function Declaration
+function sayBye(){
+  alert("Пока");
+}
+
+//Function Expression
 let sayHi = function() {
   alert( "Привет" );
 };
+
