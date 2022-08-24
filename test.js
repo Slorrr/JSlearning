1932c442606b4d9537f2d1d7e81a9759df9db6bd
Slorrr <22tamikill11@gmail.com>
2022-08-24 22:19:59 +0300

    add checking integer and fractional numbers test

--- test.js
+++ test.js
@@ -29,5 +29,15 @@ describe("pow", function() {
         }
     
       });
+
+      describe("pow", function() {
+        it("для отрицательных n возвращает NaN", function() {
+          assert.isNaN(pow(2, -1));
+        });
+      
+        it("для дробных n возвращает NaN", function() {
+          assert.isNaN(pow(2, 1.5));
+        });
+      });
 });
 
