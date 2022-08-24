fe6a648597fe870aaae00b8cf7e37c2eb7460875
Slorrr <22tamikill11@gmail.com>
2022-08-24 18:39:54 +0300

    correct implementation with improved function

--- test.js
+++ test.js
@@ -1,11 +1,14 @@
 describe("pow", function() {
 
-    it("2 в степени 3 будет 8", function() {
-      assert.equal(pow(2, 3), 8);
-    });
+    function makeTest(x) {
+      let expected = x * x * x;
+      it(`${x} в степени 3 будет ${expected}`, function() {
+        assert.equal(pow(x, 3), expected);
+      });
+    }
   
-    it("3 в степени 3 будет 27", function() {
-      assert.equal(pow(3, 3), 27);
-    });
+    for (let x = 1; x <= 5; x++) {
+      makeTest(x);
+    }
   
   });
