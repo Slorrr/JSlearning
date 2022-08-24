01ef467ad2260c2bb957635e599a3ace1c2c409d
Slorrr <22tamikill11@gmail.com>
2022-08-24 18:56:02 +0300

    Nested describe blocks

--- test.js
+++ test.js
@@ -1,14 +1,33 @@
 describe("pow", function() {
 
-    function makeTest(x) {
-      let expected = x * x * x;
-      it(`${x} в степени 3 будет ${expected}`, function() {
-        assert.equal(pow(x, 3), expected);
+    describe("возводит x в степень 3", function() {
+
+        function makeTest(x) {
+          let expected = x * x * x;
+          it(`${x} в степени 3 будет ${expected}`, function() {
+            assert.equal(pow(x, 3), expected);
+          });
+        }
+    
+        for (let x = 1; x <= 5; x++) {
+          makeTest(x);
+        }
+    
       });
-    }
-  
-    for (let x = 1; x <= 5; x++) {
-      makeTest(x);
-    }
-  
-  });
+    
+      describe("возводит x в степень 5", function() {
+
+        function makeTest(x) {
+          let expected = x * x * x * x * x;
+          it(`${x} в степени 5 будет ${expected}`, function() {
+            assert.equal(pow(x, 5), expected);
+          });
+        }
+    
+        for (let x = 1; x <= 5; x++) {
+          makeTest(x);
+        }
+    
+      });
+});
+
