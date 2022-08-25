281f12d22481014f1245c8d76050cf22a33d9fb6
Slorrr <22tamikill11@gmail.com>
2022-08-25 14:10:02 +0300

    constructor function

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let user = {
-    name: "Никита",
-    age: 17
-  };
-  
-  user.sayHi = function() {
-    alert(`Привет, я ${this.name}`);
-  };
-  
-  user.sayHi(); // Привет!
+function User(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+let User1 = new User("Nikita", 15);
+let User2 = new User("Ivan", 28);
+
+alert(User1.name + " " + User1.age);
+alert(User2.name + " " + User2.age);
