02685dc2bf72d0a47d92c984eaa9d72bcd6fbf12
Slorrr <22tamikill11@gmail.com>
2022-08-27 15:46:28 +0300

    overriding methods

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
 class Animal {
+
     constructor(name) {
       this.speed = 0;
       this.name = name;
     }
+  
     run(speed) {
       this.speed = speed;
       alert(`${this.name} бежит со скоростью ${this.speed}.`);
     }
+  
     stop() {
       this.speed = 0;
       alert(`${this.name} стоит.`);
     }
+  
   }
   
-  // Наследуем от Animal указывая "extends Animal"
   class Rabbit extends Animal {
     hide() {
       alert(`${this.name} прячется!`);
+    }
+  
+    stop() {
+      super.stop(); // вызываем родительский метод stop
+      this.hide(); // и затем hide
     }
   }
   
   let rabbit = new Rabbit("Белый кролик");
   
-  rabbit.run(5); 
+  rabbit.run(5); // Белый кролик бежит со скоростью 5.
+  rabbit.stop(); // Белый кролик стоит. Белый кролик прячется!
