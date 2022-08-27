38050ee78f9a822e482593e9a1d231515e485187
Slorrr <22tamikill11@gmail.com>
2022-08-27 14:46:53 +0300

    object to json

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function User(name, age){
-    this.name = name;
-    this.age = age;
-}
-
-let User1 = new User("Nikita", 15);
-let User2 = new User("Ivan", 28);
-
-
-alert(User1.name + " " + User1.age);
-alert(User2.name + " " + User2.age);
-
+let student = {
+    name: 'John',
+    age: 30,
+    isAdmin: false,
+    courses: ['html', 'css', 'js'],
+    wife: null
+  };
+  
+  let json = JSON.stringify(student);
+  
+  alert(typeof json); 
+  
+  alert(json);
